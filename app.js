c297a47f2fb10158dda0a3aff9e612e96cfc09e0
sigitfrank <sigittt53@gmail.com>
2018-05-22 11:34:24 +0700

    sig, geodirection, chart firebase
    menambahkan fungsi geodirection,  dapat mengatur jenis rute yang akan dilalui, membuat chart terhubung dengan firebase

---
+++ app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDhmspkmgaeJNHUA-TEoiecq87fa2lIGz8",
+    authDomain: "belajar-chart.firebaseapp.com",
+    databaseURL: "https://belajar-chart.firebaseio.com",
+    projectId: "belajar-chart",
+    storageBucket: "",
+    messagingSenderId: "439225952232"
+  };
+  firebase.initializeApp(config);
